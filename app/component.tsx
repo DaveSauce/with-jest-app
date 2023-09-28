b15fd5bdf115edfce2c03a13af25e461d3573137
davesauce <dave@cumulocreative.com>
2023-09-28 11:29:33 -0400

    add example text

--- app/component.tsx
+++ app/component.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function Counter() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   return (
     <>
       <h2>{count}</h2>
+      <p>Example text</p>
       <button type="button" onClick={() => setCount(count + 1)}>
         +
       </button>
     </>
-  )
+  );
 }
